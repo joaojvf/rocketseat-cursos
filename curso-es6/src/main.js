7f78e57fd2d451dea9556897ba45c0cf91a05408
João Victor Ferreira <30957280+joaojvf@users.noreply.github.com>
2019-07-28 20:50:54 -0300

    Conclusão do curso e projeto final

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,35 +1,93 @@
-import axios from 'axios';
+import api from './api';
 
-class Api {
-    static async getUserInfo(username){
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        console.log(response);
+class App {
+    constructor(){
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+        this.inputEl = document.querySelector('input[name=repository]')
+        this.registerHandlers();
+    }
 
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepositories(event);
+    }
+
+    setLoading(loading = true){
+        if(loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        }else{
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepositories(event){
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0 ){
+            return;
+        }
+
+        this.setLoading();
+
+        try{
+            const response = await api.get(`/repos/${repoInput}`);
+
+            const {name, description, html_url, owner: {avatar_url}} = response.data;
+    
+            console.log(response);
+    
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            
+            this.inputEl.value = "";
+            this.render();
+        } catch(err){
+            alert('Repositório Inexistente!');
+        }
+
+        this.setLoading(false);
+        
+    }    
+
+    render() {
+        this.listEl.innerHTML = "";
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url)
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+            
+            let descriptionEl = document.createElement('p');
+           descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            console.log(listItemEl);
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-Api.getUserInfo('joaojvf');
-
-const minhaPromise = () => new Promise((resolve, reject) =>
-{
-    setTimeout(() => { resolve('OK')  
-    }, 2000);
-});
-
-// minhaPromise().then(response => {
-//     console.log(response);
-// })
-// .catch(err =>{
-
-// });
-
-
-async function executaPromise(){
-    const response = await minhaPromise();
-    console.log(response) //2 segundos
-
-    console.log(await minhaPromise()); //4segundos
-    console.log(await minhaPromise());//6segundos
-}
-
-executaPromise();
+new App();
