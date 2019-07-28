2a1ac12bb7691874530cb2dca686d07272da6196
João Victor Ferreira <30957280+joaojvf@users.noreply.github.com>
2019-07-28 15:20:23 -0300

    Sessão 3

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,1 +1,35 @@
-alert('2');
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username){
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response);
+
+    }
+}
+
+Api.getUserInfo('joaojvf');
+
+const minhaPromise = () => new Promise((resolve, reject) =>
+{
+    setTimeout(() => { resolve('OK')  
+    }, 2000);
+});
+
+// minhaPromise().then(response => {
+//     console.log(response);
+// })
+// .catch(err =>{
+
+// });
+
+
+async function executaPromise(){
+    const response = await minhaPromise();
+    console.log(response) //2 segundos
+
+    console.log(await minhaPromise()); //4segundos
+    console.log(await minhaPromise());//6segundos
+}
+
+executaPromise();
